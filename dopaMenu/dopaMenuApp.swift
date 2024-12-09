6e58a8fd3043982cd06d85e25da031ef376e7a5c
Gaia Salzano <gaiasalzano18@icloud.it>
2024-12-09 11:22:13 +0100

    Initial Commit

---
+++ dopaMenu/dopaMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  dopaMenuApp.swift
+//  dopaMenu
+//
+//  Created by Gaia Salzano on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct dopaMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
